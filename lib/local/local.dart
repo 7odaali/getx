e2ccfccb9d6c0e24ae44b97985c4081efb6cf7d0
RTX <7odaaliebaideng@gmail.com>
2025-02-16 01:35:21 +0200

    .

--- lib/local/local.dart
+++ lib/local/local.dart
@@ -3,14 +3,15 @@ import 'package:get/get.dart';
 class MyLocale extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-    'ar': {
-      '1': 'الصفحه الرئيسيه',
-      '2':'عربي','3':'اتجليزي',
-    },
-    'en': {
-      '1': 'homepage',
-      '2':'arabic',
-      '3':'english',
-    }
-  };
+        'ar': {
+          '1': 'الصفحه الرئيسيه',
+          '2': 'عربي',
+          '3': 'اتجليزي',
+        },
+        'en': {
+          '1': 'homepage',
+          '2': 'arabic',
+          '3': 'english',
+        }
+      };
 }
--- lib/local/local_controller.dart
+++ lib/local/local_controller.dart
@@ -2,10 +2,8 @@ import 'dart:ui';
 
 import 'package:get/get.dart';
 
-class MyLocaleController extends GetxController{
-
-
-  void changeLang (String codelang){
+class MyLocaleController extends GetxController {
+  void changeLang(String codelang) {
     Locale locale = Locale(codelang);
     Get.updateLocale(locale);
   }
